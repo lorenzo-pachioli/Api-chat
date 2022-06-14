e95ae43ed7d65b746c1ae119706ebc549130721f
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-06-14 15:58:39 -0300

    tested and fixed signIn,logIn,logOut,delete

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,13 +2,12 @@ const User = require("../models/User");
 const Message = require("../models/Message");
 
 
-module.exports.signup = async(data,io)=>{
+module.exports.signUp = async(data,io, id)=>{
     try{
         const { firstName, lastName, email, password } = data;
         const userCheck = await User.findOne({ email: email });
         if (userCheck){
-            io.sockets.to(io.socket.id).emit("sign_up_res", { msg: "Email already used", status: false });
-            return  socket.disconnect(true)
+            return io.to(id).emit("sign_up_res", { msg: "Email already used", status: false });
         }
         /* const hashedPassword = await bcrypt.hash(password, 10); */
         const newUser = User({
@@ -18,56 +17,68 @@ module.exports.signup = async(data,io)=>{
             password: password 
         })
         newUser.save()
-        .then(response => io.sockets.to(io.socket.id).emit("sign_up_res", { status: true, response }))
-        .catch(err => io.sockets.to(io.socket.id).emit("sign_up_res", {msj: "Error saving new user", status: false, error: err}));
+        .then(response => io.to(id).emit("sign_up_res", { status: true, response }))
+        .catch(err => io.to(id).emit("sign_up_res", {msj: "Error saving new user", status: err.status, error: err}));
     }catch(err){
-        err => io.sockets.to(io.socket.id).emit("sign_up_res", {msj: "Error creating new user", status: false, error: err});
+        err => io.to(id).emit("sign_up_res", {msj: "Error creating new user", status: err.status, error: err});
     }
 }
 
-module.exports.login = async (data,io)=>{
+module.exports.logIn = async (data,io, id)=>{
     try{
         const { email, password } = data;
         const userCheck = await User.findOne({ email: email });
-        if(!userCheck){
-            io.sockets.to(io.socket.id).emit("log_in_res", { msg: "Wrong email ", status: false });
-            return  socket.disconnect(true)
-        }else if(userCheck.password !== password){
-            io.sockets.to(io.socket.id).emit("log_in_res", { msg: "Wrong password", status: false });
-            return socket.disconnect(true)
+        console.log(userCheck)
+        if(!userCheck._id){
+            io.to(id).emit("log_in_res", { msg: "Wrong email ", status: false });
+            return console.log("Wrong email ")
+        }
+        if(userCheck.password !== Number(password)){
+            io.to(id).emit("log_in_res", { msg: "Wrong password", status: false });
+            return console.log("Wrong password");
         }
 
         try{
-            const docRef =await Message.find()
-            io.sockets.to(io.socket.id).emit("log_in_res", docRef)
+            const docRef =await Message.find({})
+            io.to(id).emit("log_in_res", { status: true, user: userCheck, messages: docRef })
         }catch(err){
-            io.sockets.to(io.socket.id).emit("log_in_res", {error: err})
+            io.to(id).emit("log_in_res", {error: err})
         }
     }catch(err){
-        io.sockets.to(io.socket.id).emit("log_in_res", { msg: "Error loging in", error:err, status: false });
+        io.to(id).emit("log_in_res", { msg: "Error loging in", error:err, status: false });
     }
 }
 
-module.exports.logOut = async (io)=>{
+module.exports.logOut = async (io, socket)=>{
     try{
-        socket.disconnect(true)
+        if(socket.connected){
+            socket.disconnect(true)
+        }
+
     }catch(err){
-        io.sockets.to(io.socket.id).emit("log_out_res", { msg: "Error disconnecting", error:err, status: false });
+        io.to(socket.id).emit("log_out_res", { msg: "Error disconnecting", error:err, status: err.status });
 
     }
 }
 
-module.exports.deleteUser = async (data, io)=>{
+module.exports.deleteUser = async (data, io, socket)=>{
     try{
-        const {_id} = data;
-        if(!io.socket.connected){
-            io.sockets.to(io.socket.id).emit("delete_user_res", { msg: "Must be connected to delete user", status: false });
+        const {_id, password} = data;
+        const userCheck = await User.findById(_id);
+        if(userCheck.password !== Number(password)){
+            io.to(socket.id).emit("delete_user_res", { msg: "Wrong password", status: false });
+            return console.log("Wrong password");
         }
-        User.findByIdAndDelete({_id:_id})
-        then(()=> socket.disconnect(true))
-        .catch(err=> io.sockets.to(io.socket.id).emit("delete_user_res", { msg: "Error deleting user",error:err, status: false }))
+        try{
+            const docRef =await User.findByIdAndDelete(_id)
+            io.to(socket.id).emit("delete_user_res", { msg: "User deleted succesfully", user: docRef, status: 200 });
+            socket.disconnect(true)
+        }catch(err){
+            err=> io.to(socket.id).emit("delete_user_res", { msg: "Error deleting user",error:err, status: err.status })
+        }
+        
     }catch(err){
-        io.sockets.to(io.socket.id).emit("log_out_res", { msg: "Error dsiconnecting", error:err, status: false });
+        io.to(id).emit("log_out_res", { msg: "Error dsiconnecting", error:err, status: err.status });
     }
 }
 
--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const {Server} = require('socket.io')
 const Message = require('./models/Message');
 const User = require('./models/User')
 const {
-    sigUp,
+    signUp,
     logIn,
     logOut, 
-    deleteUser
+    deleteUser,
 } = require('./Controllers/userController');
 
 app.use(express.json())
@@ -30,38 +30,12 @@ const io = new Server(server, {
 io.on("connect", (socket)=> {
     console.log('id', socket.id)
 
-    socket.on("sign_up", async (data) => {
-        try{
-            console.log(data)
-            const { firstName, lastName, email, password } = data;
-            const userCheck = await User.findOne({ email: email });
-            console.log(userCheck)
-            if (userCheck){
-                return io.to(socket.id).emit("sign_up_res", { msg: "Email already used", status: false });
-                
-            }
-            /* const hashedPassword = await bcrypt.hash(password, 10); */
-            const newUser = User({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password 
-            })
-            newUser.save()
-            .then(response => {
-                console.log('response', response)
-                io.to(socket.id).emit("sign_up_res", { status: true, response })
-            })
-            .catch(err =>io.to(socket.id).emit("sign_up_res", {msj: "Error saving new user", status: false, error: err}));
-        }catch(err){
-            err => io.to(io.socket.id).emit("sign_up_res", {msj: "Error creating new user", status: false, error: err});
-        }
-    });
+    socket.on("sign_up", (data) => signUp(data, io, socket.id));
 
 
-    socket.on("log_in", data => logIn(data,io));
-    socket.on("log_out", data => logOut(data,io));
-    socket.on("delete_user", data => deleteUser(data,io));
+    socket.on("log_in", data => logIn(data,io, socket.id));
+    socket.on("log_out", () => logOut(io, socket));
+    socket.on("delete_user", data => deleteUser(data,io, socket));
 
     socket.once("join_room",async (data)=>{
         socket.join(data)

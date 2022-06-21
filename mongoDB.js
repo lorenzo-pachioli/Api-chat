4b63e65a882a0b3f3801fe8488987dd8e36d92e6
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-06-20 21:42:51 -0300

    test uri

--- mongoDB.js
+++ mongoDB.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const uri = process.env.MONGODB_URI;
+console.log(uri)
 mongoose.connect(uri)
 .then(() => console.log('Conected'))
 .catch(error => console.error(error))

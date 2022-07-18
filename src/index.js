f353ccacbcf0215d71434706e47799185a2cf124
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-07-18 10:49:28 -0300

    example error handled with process.on added to index.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const http = require('http');
 const cors = require("cors");
 const { Server } = require('socket.io');
 const {api} = require('./api/index');
+const {toEvent} = require('./api/helper/SocketUtils');
 app.use(express.json());
 app.use(cors());
 
@@ -21,7 +22,18 @@ io.on("connect", (socket) => {
     api(io, socket)
     console.log('2');
 });
-console.log('3');
+
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason );
+    toEvent(`log_in_res`, { msg: `Unhandled Rejection at: ${reason}`, status: false });
+});
+
+process.on('uncaughtException', (err, origin) => {
+    console.log(`Caught exception: ${err}`, ` origin: ${ origin}`);
+    toEvent(`log_in_res`, { msg: `Caught exception: ${err}`, status: false });
+});
+
 const port = process.env.PORT || 3001;
 server.listen(port, (error) => {
     console.log('Connected');

c870a43d868dad721d7a8dce22019ccf271d0a5b
lorenzo-pachioli <lorenzopachioli@gmail.com>
2026-06-10 13:47:35 -0300

    fix: sameSite to none

--- src/api/controller/AuthController.js
+++ src/api/controller/AuthController.js
@@ -89,7 +89,7 @@ exports.logInController2 = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax', // 'strict' bloqueaba la cookie en el refresh de página
+            sameSite: 'none', // 'strict' bloqueaba la cookie en el refresh de página
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
